955b9c57f3bdfb01948fa89b390d5f8af6991b70
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-02-24 15:17:13 +0200

    put the navbar with the header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,18 @@
+import './app.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import NavBar from './NavBar';
-import Word from './WordForm/WordForm';
-import RandomWordPos from './RandomWordPos';
-import RandomWordPosStartWith from './RandomWordPosStartWith';
+import HomePage from '../HomePage';
+import NavBar from '../NavBar';
+import Word from '../WordForm/WordForm';
+import RandomWordPos from '../RandomWordPos';
+import RandomWordPosStartWith from '../RandomWordPosStartWith';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1 className="header">Dictionary</h1>
-        <NavBar />
+        <h1 className="header">
+          <NavBar /> Dictionary
+        </h1>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/word/:word" element={<Word />} />
